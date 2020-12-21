503498f319042e36333009cdaa1170d66b35e295
Ankit Mehta <ankitmehta94@gmail.com>
2020-12-21 11:07:24 -0500

    updated access sheets and convert ti json

---
+++ convertRowsToJson.js
@@ -0,0 +1,13 @@
+module.exports = (rows) => {
+    const headerRow = rows.shift();
+    jsonArray = [];
+    rows.forEach((rowArray) => {
+        const rowObject = {};
+        console.log(rowArray, '<--------------------rowArray')
+        rowArray.forEach((rowElement, index) => {
+            rowObject[headerRow[index]] = rowElement;
+        })
+        jsonArray.push(rowObject);
+    });
+    return jsonArray;
+}

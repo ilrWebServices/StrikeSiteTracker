7f06bd17cab1e73c255066375ffcda396282d5f0
Ankit Mehta <ankitmehta94@gmail.com>
2021-01-11 19:47:14 -0500

    fixed api key issue

--- findLatLng.js
+++ findLatLng.js
@@ -1,7 +1,7 @@
 const fs  = require('fs');
 const axios = require('axios')
 var _get = require('lodash.get');
-
+const GEOCODING_API = process.env.GEOCODING_API
 const convertLatLngObjectToString = (latlngObj) => {
     if(latlngObj)
         return `${latlngObj.lat},${latlngObj.lng}`
@@ -19,7 +19,7 @@ module.exports  = async (content) => {
             const address = `${element.Address.trim()}, ${element['Zip Code']}, ${element.City}, ${element.State}`
             const addressURI= address.replace(/ /g,"+")
             // console.log(addressURI)
-            const result = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addressURI}&key=AIzaSyAZrTp6Gtg0savyY6KhaT-ibWIE-0WeXXc`)
+            const result = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addressURI}&key=${GEOCODING_API}`)
             const latlngObj = _get(result,'data.results[0].geometry.location',null)
             // console.log(latlngObj)
             element['Latitude, Longitude'] = convertLatLngObjectToString(latlngObj)

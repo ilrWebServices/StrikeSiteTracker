beeac163946fac2e9d6ccf3ea53d6228508b378e
David DeMello <daviddemello@gmail.com>
2024-02-02 11:15:59 -0500

    Increases range of spreadsheet cells loaded into memory.

--- build.js
+++ build.js
@@ -102,7 +102,7 @@ function listMajors(auth) {
   sheets.spreadsheets.values.get(
     {
       spreadsheetId: spreadsheetId,
-      range: "A1:AZ3000",
+      range: "A1:AZ4000",
     },
     async (err, res) => {
       if (err) return console.log("The API returned an error: " + err);
